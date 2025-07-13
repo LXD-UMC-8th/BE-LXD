f3014bea7af8e73baa1bbfaf9acfe7caa4be7333
Seohyeon Shin <ss99x200211@gmail.com>
2025-07-13 20:06:13 +0900

    Feat : 사용자 저장 교정 Entity 추가

---
+++ src/main/java/org/lxdproject/lxd/correction/entity/mapping/MemberSavedCorrection.java
@@ -0,0 +1,29 @@
+package org.lxdproject.lxd.correction.entity.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.lxdproject.lxd.correction.entity.Correction;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberSavedCorrection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "correction_id", nullable = false)
+    private Correction correction;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id", nullable = false)
+//    private Member member;
+
+    @Column(name = "memo")
+    private String memo;
+}
+
