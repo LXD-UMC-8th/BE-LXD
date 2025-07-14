d6a7c38690d1c8372ef4207db4903a18659d9a86
eclipse021 <yahang2008@naver.com>
2025-07-15 02:42:28 +0900

    Docs: SecurityUtil.getCurrentMemberId() JavaDoc 작성

--- src/main/java/org/lxdproject/lxd/config/security/SecurityUtil.java
+++ src/main/java/org/lxdproject/lxd/config/security/SecurityUtil.java
@@ -9,6 +9,13 @@ import org.springframework.security.core.userdetails.User;
 
 public class SecurityUtil {
 
+    /**
+     * 현재 로그인된 사용자의 memberId를 반환합니다.
+     * 인증되지 않은 경우 예외를 발생시킵니다.
+     *
+     * @return 로그인된 사용자의 memberId
+     * @throws AuthHandler 인증 정보가 없거나 잘못된 경우
+     */
     public static Long getCurrentMemberId() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -20,7 +27,7 @@ public class SecurityUtil {
             String principalStr = authentication.getName();
             return Long.valueOf(principalStr);
         } catch (Exception e) {
-            throw new AuthHandler(ErrorStatus.INVALID_AUTHENTICATION_INFO);
+            throw new AuthHandler(ErrorStatus.INVALID_TOKEN);
         }
     }
 
