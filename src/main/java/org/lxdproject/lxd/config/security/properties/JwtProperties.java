42fb8e42890d505d4e9a7178f8caeee5b8323b17
eclipse021 <yahang2008@naver.com>
2025-07-15 00:16:04 +0900

    Feat: JwtProperties 작성

--- src/main/java/org/lxdproject/lxd/config/security/properties/JwtProperties.java
+++ src/main/java/org/lxdproject/lxd/config/security/properties/JwtProperties.java
@@ -1,4 +1,32 @@
 package org.lxdproject.lxd.config.security.properties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.redis.core.types.Expiration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties("jwt")
 public class JwtProperties {
+    private String secret="";
+
+    private AccessToken accessToken;
+    private RefreshToken refreshToken;
+
+    @Getter
+    @Setter
+    public static class AccessToken {
+        private Long expiration;
+    }
+
+    @Getter
+    @Setter
+    public static class RefreshToken {
+        private Long expiration;
+    }
+
 }
