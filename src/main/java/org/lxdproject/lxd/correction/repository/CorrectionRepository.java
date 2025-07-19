849f07f0c940e0938a09b15eb02da104a18f05e7
Seohyeon Shin <ss99x200211@gmail.com>
2025-07-19 19:25:20 +0900

    Feat : 내가 제공한 교정 목록 조회 API 구현

--- src/main/java/org/lxdproject/lxd/correction/repository/CorrectionRepository.java
+++ src/main/java/org/lxdproject/lxd/correction/repository/CorrectionRepository.java
@@ -1,7 +1,11 @@
 package org.lxdproject.lxd.correction.repository;
 
 import org.lxdproject.lxd.correction.entity.Correction;
+import org.lxdproject.lxd.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CorrectionRepository extends JpaRepository<Correction, Long> {
+    Page<Correction> findByAuthor(Member author, Pageable pageable);
 }
