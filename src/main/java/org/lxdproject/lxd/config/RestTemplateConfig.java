fe4e84e825d22b0110a2d2912e2bafe9c8770d12
eclipse021 <yahang2008@naver.com>
2025-07-25 03:28:13 +0900

    Feat: RestTemplateConfig 생성

---
+++ src/main/java/org/lxdproject/lxd/config/RestTemplateConfig.java
@@ -0,0 +1,16 @@
+package org.lxdproject.lxd.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/* RestTemplate 빈 등록을 위한 config */
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
