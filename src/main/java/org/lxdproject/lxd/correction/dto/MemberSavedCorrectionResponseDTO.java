c5d185b3f4d6f400fc42745abf296f2445685bca
Seohyeon Shin <ss99x200211@gmail.com>
2025-07-25 14:22:56 +0900

    Feat : 사용자 저장 교정 Response DTO 생성

--- src/main/java/org/lxdproject/lxd/correction/dto/MemberSavedCorrectionResponseDTO.java
+++ src/main/java/org/lxdproject/lxd/correction/dto/MemberSavedCorrectionResponseDTO.java
@@ -1,14 +1,59 @@
 package org.lxdproject.lxd.correction.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MemberSavedCorrectionResponseDTO {
+
+    @Getter
+    @Builder
+    public static class SavedListResponseDTO {
+        private Long memberId;
+        private List<SavedCorrectionItem> savedCorrections;
+        private int page;
+        private int size;
+        private boolean hasNext;
+
+        @Getter
+        @Builder
+        public static class SavedCorrectionItem {
+            private Long savedCorrectionId;
+            private String memo;
+            private CorrectionInfo correction;
+            private DiaryInfo diary;
+            private AuthorInfo author;
+
+            @Getter
+            @Builder
+            public static class CorrectionInfo {
+                private Long correctionId;
+                private String originalText;
+                private String corrected;
+                private String commentText;
+                private String correctionCreatedAt;
+            }
+
+            @Getter
+            @Builder
+            public static class DiaryInfo {
+                private Long diaryId;
+                private String diaryTitle;
+                private String diaryCreatedAt;
+            }
+
+            @Getter
+            @Builder
+            public static class AuthorInfo {
+                private Long memberId;
+                private String userId;
+                private String nickname;
+                private String profileImageUrl;
+            }
+        }
+    }
 
     @Getter
     @Builder
