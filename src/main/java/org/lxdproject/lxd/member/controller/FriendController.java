ee8efc4342bb90e946939390024d0b652cc6e16e
김민지 <minzikx@sookmyung.ac.kr>
2025-07-29 00:32:06 +0900

    Feat: controller 코드 작성

---
+++ src/main/java/org/lxdproject/lxd/member/controller/FriendController.java
@@ -0,0 +1,25 @@
+package org.lxdproject.lxd.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.lxdproject.lxd.apiPayload.ApiResponse;
+import org.lxdproject.lxd.config.security.SecurityUtil;
+import org.lxdproject.lxd.member.dto.FriendListResponseDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/friends")
+@RequiredArgsConstructor
+public class FriendController {
+
+    private final FriendService friendService;
+
+    @GetMapping
+    public ApiResponse<FriendListResponseDTO> getFriendList() {
+        Long currentMemberId = SecurityUtil.getCurrentMemberId();
+        FriendListResponseDTO response = friendService.getFriendList(currentMemberId);
+        return ApiResponse.onSuccess(response);
+    }
+}
