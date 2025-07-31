a436728aee28e3c9c20f7d96f379139893500a2d
김민지 <minzikx@sookmyung.ac.kr>
2025-07-31 15:26:31 +0900

    Feat: diaryRepository에 countByMemberId 함수 추가

--- src/main/java/org/lxdproject/lxd/diary/repository/DiaryRepository.java
+++ src/main/java/org/lxdproject/lxd/diary/repository/DiaryRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long>, DiaryRepositoryCustom {
     Optional<Diary> findByIdAndDeletedAtIsNull(Long id);
+    Long countByMemberId(Long memberId);
 }
