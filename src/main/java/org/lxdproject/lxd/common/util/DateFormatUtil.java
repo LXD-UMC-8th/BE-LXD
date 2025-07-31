b54d81d677e405de541391e023d0fcd669294d2a
happine2s <happine2s@sookmyung.ac.kr>
2025-08-01 03:38:52 +0900

    Fix: 날짜 포맷 패턴 수정

--- src/main/java/org/lxdproject/lxd/common/util/DateFormatUtil.java
+++ src/main/java/org/lxdproject/lxd/common/util/DateFormatUtil.java
@@ -2,10 +2,9 @@ package org.lxdproject.lxd.common.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 public class DateFormatUtil {
     public static String formatDate(LocalDateTime dateTime) {
-        return dateTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd H:M"));
+        return dateTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
     }
 }
