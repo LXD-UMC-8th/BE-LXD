cd479f46a4268da90e9bb37e4cd731659c4998e9
forlyby <forlyby@naver.com>
2025-07-31 20:33:28 +0900

    Refactor: 교정댓글 repository 수정

--- src/main/java/org/lxdproject/lxd/correctioncomment/repository/CorrectionCommentRepository.java
+++ src/main/java/org/lxdproject/lxd/correctioncomment/repository/CorrectionCommentRepository.java
@@ -7,11 +7,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CorrectionCommentRepository extends JpaRepository<CorrectionComment, Long> {
+
     Page<CorrectionComment> findAllByCorrectionId(Long correctionId, Pageable pageable);
     @Query("SELECT cc.correction.diary.title FROM CorrectionComment cc WHERE cc.id = :id AND cc.correction IS NOT NULL AND cc.correction.diary IS NOT NULL")
     Optional<String> findDiaryTitleByCorrectionCommentId(@Param("id") Long id);
+
+
+    // 부모 댓글 페이징
+    Page<CorrectionComment> findByCorrectionIdAndParentIsNull(Long correctionId, Pageable pageable);
+
+    // 자식 댓글 일괄 조회
+    List<CorrectionComment> findByCorrectionIdIn(List<Long> parentIds);
+
+    List<CorrectionComment> findByParentIdIn(List<Long> parentIds);
+
 }
 
