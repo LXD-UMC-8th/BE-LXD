715f231ba3398592f581f2177c0aded6ca877dec
eclipse021 <yahang2008@naver.com>
2025-08-05 10:28:36 +0900

    Feat: 일기 좋아요 컨트롤러 메서드 작성

--- src/main/java/org/lxdproject/lxd/diarylike/controller/DiaryLikeApi.java
+++ src/main/java/org/lxdproject/lxd/diarylike/controller/DiaryLikeApi.java
@@ -1,7 +1,30 @@
 package org.lxdproject.lxd.diarylike.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.lxdproject.lxd.apiPayload.ApiResponse;
+import org.lxdproject.lxd.diarylike.dto.DiaryLikeResponseDTO;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Tag(name = "Diary Like", description = "일기 좋아요 관련 API 입니다.")
+@RequestMapping("/diaries")
 public interface DiaryLikeApi {
 
-
+    @PostMapping("/{diaryId}/likes")
+    @Operation(summary = "일기 좋아요 API")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "일기 좋아요 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "존재하지 않는 리소스입니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
+    ApiResponse<DiaryLikeResponseDTO.ToggleDiaryLikeResponseDTO> toggleDiaryLike(
+            @Parameter(description = "일기 ID") @PathVariable Long diaryId
+    );
 }
