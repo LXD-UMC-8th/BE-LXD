234587190de9c131efd520ec7efdbb45a27727d7
eclipse021 <yahang2008@naver.com>
2025-08-05 14:45:01 +0900

    Feat: 누락된 @PathVariable 추가

--- src/main/java/org/lxdproject/lxd/diarylike/controller/DiaryLikeController.java
+++ src/main/java/org/lxdproject/lxd/diarylike/controller/DiaryLikeController.java
@@ -5,6 +5,7 @@ import org.lxdproject.lxd.apiPayload.ApiResponse;
 import org.lxdproject.lxd.diarylike.dto.DiaryLikeResponseDTO;
 import org.lxdproject.lxd.diarylike.service.DiaryLikeService;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +16,7 @@ public class DiaryLikeController implements DiaryLikeApi {
     private final DiaryLikeService diaryLikeService;
 
     @Override
-    public ApiResponse<DiaryLikeResponseDTO.ToggleDiaryLikeResponseDTO> toggleDiaryLike(Long diaryId) {
+    public ApiResponse<DiaryLikeResponseDTO.ToggleDiaryLikeResponseDTO> toggleDiaryLike(@PathVariable Long diaryId) {
 
         DiaryLikeResponseDTO.ToggleDiaryLikeResponseDTO toggleDiaryLikeResponseDTO = diaryLikeService.toggleDiaryLike(diaryId);
         return ApiResponse.onSuccess(toggleDiaryLikeResponseDTO);
