c6f777be0619197137e596b60faa7dfe5f8bf381
happine2s <happine2s@sookmyung.ac.kr>
2025-08-09 11:01:51 +0900

    Feat: 프리플라이트(OPTIONS) 응답에 CORS 헤더 추가

--- src/main/java/org/lxdproject/lxd/config/security/SecurityConfig.java
+++ src/main/java/org/lxdproject/lxd/config/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.lxdproject.lxd.config.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -63,6 +64,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(WHITELIST).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
