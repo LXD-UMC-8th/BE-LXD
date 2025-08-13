795c8794e4fcf4d4cb988abc76ae04c844b139c8
happine2s <happine2s@sookmyung.ac.kr>
2025-08-13 22:27:34 +0900

    Chore: Image 엔티티 제거

--- src/main/java/org/lxdproject/lxd/common/service/ImageService.java
+++ src/main/java/org/lxdproject/lxd/common/service/ImageService.java
@@ -2,30 +2,19 @@ package org.lxdproject.lxd.common.service;
 
 import lombok.RequiredArgsConstructor;
 import org.lxdproject.lxd.common.dto.ImageResponseDTO;
-import org.lxdproject.lxd.common.entity.Image;
 import org.lxdproject.lxd.common.entity.enums.ImageDir;
-import org.lxdproject.lxd.common.repository.ImageRepository;
 import org.lxdproject.lxd.infra.storage.S3FileService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
 
     private final S3FileService s3FileService;
-    private final ImageRepository imageRepository;
 
     public ImageResponseDTO uploadImage(MultipartFile file, ImageDir dirName) {
         String url = s3FileService.uploadImage(file, dirName.getDirName());
-        Image saved = imageRepository.save(
-                Image.builder()
-                        .imageUrl(url)
-                        .dir(dirName) // enum 자체 저장
-                        .build()
-        );
         return new ImageResponseDTO(url);
     }
 }
