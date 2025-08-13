c173fa8e89775dd0b98ff205fbe82fc52a2e16eb
happine2s <happine2s@sookmyung.ac.kr>
2025-08-14 00:42:45 +0900

    Feat: s3properties 유효성 검증 추가로 설정 누락을 조기 탐지

--- src/main/java/org/lxdproject/lxd/config/properties/S3Properties.java
+++ src/main/java/org/lxdproject/lxd/config/properties/S3Properties.java
@@ -1,15 +1,20 @@
 package org.lxdproject.lxd.config.properties;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "aws.s3")
+@Validated
 public class S3Properties {
+    @NotBlank
     private String bucket;
+    @NotBlank
     private String defaultProfileUrl;
 }
