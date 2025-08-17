97c28d091681b501675d6d71da0dee82f0e88b83
happine2s <happine2s@sookmyung.ac.kr>
2025-08-17 22:33:24 +0900

    Fix: DTO 변환시 N+1 방지를 위해 fechjoin 적용

--- src/main/java/org/lxdproject/lxd/diarycomment/repository/DiaryCommentRepositoryImpl.java
+++ src/main/java/org/lxdproject/lxd/diarycomment/repository/DiaryCommentRepositoryImpl.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.lxdproject.lxd.diarycomment.entity.DiaryComment;
 import org.lxdproject.lxd.diarycomment.entity.QDiaryComment;
 import org.lxdproject.lxd.diarycommentlike.entity.QDiaryCommentLike;
+import org.lxdproject.lxd.member.entity.QMember;
 
 import java.util.List;
 
@@ -14,11 +15,13 @@ public class DiaryCommentRepositoryImpl implements DiaryCommentRepositoryCustom 
     private final JPAQueryFactory queryFactory;
 
     QDiaryComment comment = QDiaryComment.diaryComment;
+    QMember member = QMember.member;
 
     @Override
     public List<DiaryComment> findParentComments(Long diaryId, int offset, int size) {
         return queryFactory
                 .selectFrom(comment)
+                .leftJoin(comment.member, member).fetchJoin()
                 .where(comment.diary.id.eq(diaryId)
                         .and(comment.parent.isNull()))
                 .orderBy(comment.createdAt.asc())
@@ -31,6 +34,8 @@ public class DiaryCommentRepositoryImpl implements DiaryCommentRepositoryCustom 
     public List<DiaryComment> findRepliesByParentIds(List<Long> parentIds) {
         return queryFactory
                 .selectFrom(comment)
+                .leftJoin(comment.member, member).fetchJoin()
+                .leftJoin(comment.parent).fetchJoin()
                 .where(comment.parent.id.in(parentIds))
                 .orderBy(comment.createdAt.asc())
                 .fetch();
