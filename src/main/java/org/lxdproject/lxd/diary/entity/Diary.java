074d3262d8d080ce5eae0118ab6d8a5307b15960
happine2s <happine2s@sookmyung.ac.kr>
2025-08-18 22:08:19 +0900

    Feat: 일기 객체 생성시 enum에 기본값 자동 설정

--- src/main/java/org/lxdproject/lxd/diary/entity/Diary.java
+++ src/main/java/org/lxdproject/lxd/diary/entity/Diary.java
@@ -1,6 +1,7 @@
 package org.lxdproject.lxd.diary.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -40,13 +41,19 @@ public class Diary extends BaseEntity {
     private String content;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @NotNull
     private Style style;
 
     @Enumerated(EnumType.STRING)
-    private Visibility visibility;
+    @Column(nullable = false)
+    @NotNull
+    private Visibility visibility = Visibility.PUBLIC;
 
     @Enumerated(EnumType.STRING)
-    private CommentPermission commentPermission;
+    @Column(nullable = false)
+    @NotNull
+    private CommentPermission commentPermission = CommentPermission.ALL;
 
     @Enumerated(EnumType.STRING)
     private Language language;
