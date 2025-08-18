07e7e7fdf823a184d1c2954eddfd6f64ce6d6138
happine2s <happine2s@sookmyung.ac.kr>
2025-08-18 14:28:29 +0900

    Refactor: 특정 사용자의 id로 친구의 id를 추출하는 로직을 Friend로 분리

--- src/main/java/org/lxdproject/lxd/friend/repository/FriendRepositoryCustom.java
+++ src/main/java/org/lxdproject/lxd/friend/repository/FriendRepositoryCustom.java
@@ -4,6 +4,8 @@ import org.lxdproject.lxd.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Set;
+
 public interface FriendRepositoryCustom {
     Page<Member> findFriendsByMemberId(Long memberId, Pageable pageable);
     long countFriendsByMemberId(Long memberId);
@@ -11,4 +13,5 @@ public interface FriendRepositoryCustom {
     void saveFriendship(Member requester, Member receiver);
     void softDeleteFriendship(Member m1, Member m2);
     boolean existsFriendRelation(Long memberId, Long friendId);
+    Set<Long> findFriendIdsByMemberId(Long memberId);
 }
