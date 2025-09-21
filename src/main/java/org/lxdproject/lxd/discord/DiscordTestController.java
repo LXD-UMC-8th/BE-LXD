c5ed0308e22d0a5727a9051111a88c131fc741bb
김민지 <minzikx@sookmyung.ac.kr>
2025-09-21 18:53:10 +0900

    feat: sentry 설정 및 디스코드 알림 코드 추가

---
+++ src/main/java/org/lxdproject/lxd/discord/DiscordTestController.java
@@ -0,0 +1,14 @@
+package org.lxdproject.lxd.discord;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Discord WebHook API [TEMP]", description = "Discord Webhook 연동 테스트 관련 API 입니다. [TEMP]")
+public class DiscordTestController {
+    @GetMapping("/discord-test/throw")
+    public String throwErr() {
+        throw new RuntimeException("디스코드 테스트용");
+    }
+}
