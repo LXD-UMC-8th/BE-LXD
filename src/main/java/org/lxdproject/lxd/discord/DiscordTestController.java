fab4ffe68d59c8e4d7c5d3f7218f751b17b28deb
김민지 <minzikx@sookmyung.ac.kr>
2025-09-22 13:58:06 +0900

    feat: sentry 테스트 용 엔드포인트 수정

--- src/main/java/org/lxdproject/lxd/discord/DiscordTestController.java
+++ src/main/java/org/lxdproject/lxd/discord/DiscordTestController.java
@@ -1,5 +1,6 @@
 package org.lxdproject.lxd.discord;
 
+import io.sentry.Sentry;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Discord WebHook API [TEMP]", description = "Discord Webhook 연동 테스트 관련 API 입니다. [TEMP]")
 public class DiscordTestController {
     @GetMapping("/discord-test/throw")
-    public String throwErr() {
-        throw new RuntimeException("디스코드 연동 테스트 성공");
+    public void throwErr() {
+        try {
+            throw new RuntimeException("디스코드 연동 테스트 성공");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+        }
     }
 }
