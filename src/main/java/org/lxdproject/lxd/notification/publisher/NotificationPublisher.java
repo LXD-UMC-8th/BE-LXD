1836dd3fb83dca9125a446d09da841e2a6d257cb
happine2s <happine2s@sookmyung.ac.kr>
2025-09-27 18:26:26 +0900

    Feat: 알림 객체 생성 전 먼저 이벤트 발행되지 않도록 afterCommit 적용

--- src/main/java/org/lxdproject/lxd/notification/publisher/NotificationPublisher.java
+++ src/main/java/org/lxdproject/lxd/notification/publisher/NotificationPublisher.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Slf4j
 @Component
@@ -16,6 +18,19 @@ public class NotificationPublisher {
     @Qualifier("jsonRedisTemplate")
     private final RedisTemplate<String, Object> jsonRedisTemplate;
     private final ChannelTopic notificationTopic;
+
+    public void publishAfterCommit(NotificationMessageContext message) {
+        if (TransactionSynchronizationManager.isSynchronizationActive()) {
+            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+                @Override
+                public void afterCommit() {
+                    publish(message);
+                }
+            });
+        } else {
+            publish(message);
+        }
+    }
 
     public void publish(NotificationMessageContext message) {
         String topic = notificationTopic.getTopic();
@@ -32,3 +47,4 @@ public class NotificationPublisher {
         }
     }
 }
+
