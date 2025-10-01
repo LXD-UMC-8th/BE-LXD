f3bdc3d8a9af11059feabbf5543ca50e4a8bec1d
happine2s <happine2s@sookmyung.ac.kr>
2025-10-01 14:07:29 +0900

    Feat: 일기와 멤버의 Guard 분리

--- src/main/java/org/lxdproject/lxd/authz/guard/DiaryGuard.java
+++ src/main/java/org/lxdproject/lxd/authz/guard/DiaryGuard.java
@@ -2,11 +2,9 @@ package org.lxdproject.lxd.authz.guard;
 
 import lombok.RequiredArgsConstructor;
 import org.lxdproject.lxd.apiPayload.code.exception.handler.DiaryHandler;
-import org.lxdproject.lxd.apiPayload.code.exception.handler.MemberHandler;
 import org.lxdproject.lxd.apiPayload.code.status.ErrorStatus;
 import org.lxdproject.lxd.authz.model.Permit;
 import org.lxdproject.lxd.authz.policy.DiaryPolicy;
-import org.lxdproject.lxd.authz.policy.MemberPolicy;
 import org.lxdproject.lxd.diary.entity.Diary;
 import org.springframework.stereotype.Component;
 
@@ -14,17 +12,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class DiaryGuard {
     private final DiaryPolicy diaryPolicy;
-    private final MemberPolicy memberPolicy;
     private final FriendshipQueryPort friendshipQueryPort;
 
     public void hasVisibilityPermission(Long viewerId, Diary diary) {
-
-        // 탈퇴한 사용자의 일기 조회 요청인지 검사
-        Permit ownerPermit = memberPolicy.checkDeletedMember(diary.getMember());
-        if (ownerPermit == Permit.WITHDRAWN) {
-            throw new MemberHandler(ErrorStatus.RESOURCE_OWNER_WITHDRAWN);
-        }
-
         Long ownerId = diary.getMember().getId();
         boolean areFriends = viewerId != null && friendshipQueryPort.areFriends(viewerId, ownerId);
 
--- src/main/java/org/lxdproject/lxd/authz/policy/DiaryPolicy.java
+++ src/main/java/org/lxdproject/lxd/authz/policy/DiaryPolicy.java
@@ -3,13 +3,11 @@ package org.lxdproject.lxd.authz.policy;
 import lombok.RequiredArgsConstructor;
 import org.lxdproject.lxd.authz.model.Permit;
 import org.lxdproject.lxd.diary.entity.Diary;
-import org.lxdproject.lxd.member.entity.Member;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
 public class DiaryPolicy {
-
     public Permit hasVisibilityPermission(Long viewerId, Diary diary, boolean areFriends) {
 
         Long ownerId = diary.getMember().getId();
