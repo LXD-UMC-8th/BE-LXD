71a88e2c9d4c1332a2412026af148a05673aced5
eclipse021 <yahang2008@naver.com>
2025-10-05 18:53:41 +0900

    Feat: 친구 관계 검사용 validateExistingFriends 메서드 추가

--- src/main/java/org/lxdproject/lxd/authz/policy/FriendPolicy.java
+++ src/main/java/org/lxdproject/lxd/authz/policy/FriendPolicy.java
@@ -23,14 +23,24 @@ public class FriendPolicy {
         return Permit.ALLOW;
     }
 
-    public Permit validateFriends(Member memberA, Member memberB) {
+    // 친구 관계가 존재하지 않아야 함 (요청 전 검사용)
+    public Permit validateNotFriends(Member memberA, Member memberB) {
         if (memberA == null || memberB == null) return Permit.DENY;
 
-        // 이미 친구인지 확인
+        // 두 사용자가 이미 친구라면 DENY, 아니라면 ALLOW
         boolean areFriends = friendRepository.areFriends(memberA.getId(), memberB.getId());
         if (areFriends) return Permit.DENY;
 
         return Permit.ALLOW;
+    }
+
+    // 친구 관계가 반드시 존재해야 함 (취소,거절 등 관리 전 검사용)
+    public Permit validateExistingFriends(Member memberA, Member memberB) {
+        if (memberA == null || memberB == null) return Permit.DENY;
+
+        // 두 사용자가 이미 친구라면 ALLOW, 아니라면 DENY
+        boolean areFriends = friendRepository.areFriends(memberA.getId(), memberB.getId());
+        return areFriends ? Permit.ALLOW : Permit.DENY;
     }
 
 }
