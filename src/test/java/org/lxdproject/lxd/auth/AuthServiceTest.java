e6e0075a933f42ced2a3c5f06364732d2a3c8cef
eclipse021 <yahang2008@naver.com>
2025-10-09 07:46:47 +0900

    Test: 회원 복구 시, 해당 회원이 작성한 일기에 달린 댓글/좋아요, 작성한 댓글에 달린 좋아요도 복구되는지 검증하는 테스트 코드 작성

--- src/test/java/org/lxdproject/lxd/auth/AuthServiceTest.java
+++ src/test/java/org/lxdproject/lxd/auth/AuthServiceTest.java
@@ -128,4 +128,122 @@ public class AuthServiceTest {
         SecurityContextHolder.clearContext();
     }
 
+    @Test
+    @DisplayName("회원 복구 시, 해당 회원이 작성한 일기에 달린 댓글/좋아요, 작성한 댓글에 달린 좋아요도 복구된다")
+    void recoverMember_shouldRestoreRelatedEntities() {
+        // [given] 탈퇴할 회원 A, 연관 회원 B, C
+        Member memberA = Member.builder()
+                .username("memberA_recover")
+                .password("pw")
+                .email("a@test.com")
+                .nickname("A")
+                .role(Role.USER)
+                .loginType(LoginType.LOCAL)
+                .nativeLanguage(Language.KO)
+                .language(Language.ENG)
+                .systemLanguage(Language.KO)
+                .isPrivacyAgreed(true)
+                .isAlarmAgreed(true)
+                .status(Status.ACTIVE)
+                .build();
+        memberRepository.save(memberA);
+
+        Member memberB = Member.builder()
+                .username("memberB_recover")
+                .password("pw")
+                .email("b@test.com")
+                .nickname("B")
+                .role(Role.USER)
+                .loginType(LoginType.LOCAL)
+                .nativeLanguage(Language.KO)
+                .language(Language.ENG)
+                .systemLanguage(Language.KO)
+                .isPrivacyAgreed(true)
+                .isAlarmAgreed(true)
+                .status(Status.ACTIVE)
+                .build();
+        memberRepository.save(memberB);
+
+        Member memberC = Member.builder()
+                .username("memberC_recover")
+                .password("pw")
+                .email("c@test.com")
+                .nickname("C")
+                .role(Role.USER)
+                .loginType(LoginType.LOCAL)
+                .nativeLanguage(Language.KO)
+                .language(Language.ENG)
+                .systemLanguage(Language.KO)
+                .isPrivacyAgreed(true)
+                .isAlarmAgreed(true)
+                .status(Status.ACTIVE)
+                .build();
+        memberRepository.save(memberC);
+
+        Diary diaryA = Diary.builder()
+                .member(memberA)
+                .title("A의 일기 복구")
+                .content("A의 내용")
+                .style(Style.FREE)
+                .visibility(Visibility.PUBLIC)
+                .commentPermission(CommentPermission.ALL)
+                .language(Language.KO)
+                .build();
+        diaryRepository.save(diaryA);
+
+        // B가 A의 일기에 댓글, 좋아요
+        DiaryComment commentB_on_A = DiaryComment.builder()
+                .member(memberB)
+                .diary(diaryA)
+                .commentText("B의 댓글")
+                .build();
+        diaryCommentRepository.save(commentB_on_A);
+        DiaryLike likeB_on_A = DiaryLike.builder()
+                .member(memberB)
+                .diary(diaryA)
+                .build();
+        diaryLikeRepository.save(likeB_on_A);
+
+        // A가 댓글 작성
+        DiaryComment commentA = DiaryComment.builder()
+                .member(memberA)
+                .diary(diaryA)
+                .commentText("A의 댓글")
+                .build();
+        diaryCommentRepository.save(commentA);
+
+        // C가 A의 댓글에 좋아요
+        DiaryCommentLike likeC_on_commentA = DiaryCommentLike.builder()
+                .member(memberC)
+                .comment(commentA)
+                .build();
+        diaryCommentLikeRepository.save(likeC_on_commentA);
+
+        // A 탈퇴
+        memberService.deleteMember(memberA.getId());
+
+        // [when]
+        // CustomUserDetails 객체 생성
+        CustomUserDetails customUserDetails = new CustomUserDetails(memberA);
+
+        // SecurityContext에 CustomUserDetails 주입
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        authService.recover();
+
+        // then (A의 일기에 달린 댓글/좋아요, A의 댓글에 달린 좋아요도 복구됨)
+        DiaryComment restoredCommentB = diaryCommentRepository.findById(commentB_on_A.getId()).orElseThrow();
+        DiaryLike restoredLikeB = diaryLikeRepository.findById(likeB_on_A.getId()).orElseThrow();
+        DiaryCommentLike restoredLikeC = diaryCommentLikeRepository.findById(likeC_on_commentA.getId()).orElseThrow();
+
+        assertThat(restoredCommentB.getDeletedAt()).isNull();
+        assertThat(restoredLikeB.getDeletedAt()).isNull();
+        assertThat(restoredLikeC.getDeletedAt()).isNull();
+
+        // test 끝나면 context 초기화
+        SecurityContextHolder.clearContext();
+    }
+
 }
