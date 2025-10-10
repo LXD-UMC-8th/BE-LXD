af3a0bbb5bd4a89fefca5bf63afa6cee86be474d
김민지 <minzikx@sookmyung.ac.kr>
2025-10-10 13:35:34 +0900

    feat: WebMvcConfigurer에 Resolver 등록

---
+++ src/main/java/org/lxdproject/lxd/config/web/WebConfig.java
@@ -0,0 +1,22 @@
+package org.lxdproject.lxd.config.web;
+
+import lombok.RequiredArgsConstructor;
+import org.lxdproject.lxd.annotation.resolver.AuthenticatedMemberResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticatedMemberResolver authenticatedMemberResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticatedMemberResolver);
+    }
+}
+
