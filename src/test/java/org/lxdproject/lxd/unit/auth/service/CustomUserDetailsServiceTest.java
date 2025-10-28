b476566f43487c67123fbfcd54c421b416e989ff
eclipse021 <yahang2008@naver.com>
2025-10-29 01:24:24 +0900

    Chore: CustomUserDetailsServiceTest 생성

---
+++ src/test/java/org/lxdproject/lxd/unit/auth/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,32 @@
+package org.lxdproject.lxd.unit.auth.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.lxdproject.lxd.auth.dto.CustomUserDetails;
+import org.lxdproject.lxd.auth.service.CustomUserDetailsService;
+import org.lxdproject.lxd.diary.entity.enums.Language;
+import org.lxdproject.lxd.member.entity.Member;
+import org.lxdproject.lxd.member.entity.enums.LoginType;
+import org.lxdproject.lxd.member.entity.enums.Role;
+import org.lxdproject.lxd.member.repository.MemberRepository;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+class CustomUserDetailsServiceTest {
+
+
+
+}
+
