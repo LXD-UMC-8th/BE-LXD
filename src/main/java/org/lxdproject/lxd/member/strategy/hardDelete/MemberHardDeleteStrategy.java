8d3fed1cfae05a864894b7a49d3963190d08976f
happine2s <happine2s@sookmyung.ac.kr>
2025-10-30 03:24:54 +0900

    Fix: 멤버의 HardDelete 순서 수정

--- src/main/java/org/lxdproject/lxd/member/strategy/hardDelete/MemberHardDeleteStrategy.java
+++ src/main/java/org/lxdproject/lxd/member/strategy/hardDelete/MemberHardDeleteStrategy.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
-@Order(3)
+@Order(6)
 public class MemberHardDeleteStrategy implements HardDeleteStrategy {
 
     private final MemberRepository memberRepository;
