4bfe0841d5756e6a3be2f2cc2e0ed840282d87cd
eclipse021 <yahang2008@naver.com>
2025-10-29 20:59:29 +0900

    Feat: DiarySoftDeleteStrategy 구현

---
+++ src/main/java/org/lxdproject/lxd/member/service/strategy/DiarySoftDeleteStrategy.java
@@ -0,0 +1,20 @@
+package org.lxdproject.lxd.member.service.strategy;
+
+import lombok.RequiredArgsConstructor;
+import org.lxdproject.lxd.diary.repository.DiaryRepository;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class DiarySoftDeleteStrategy implements SoftDeleteStrategy {
+
+    private final DiaryRepository diaryRepository;
+
+    @Override
+    public void softDelete(Long memberId, LocalDateTime deletedAt) {
+        // 일기 soft delete
+        diaryRepository.softDeleteDiariesByMemberId(memberId, deletedAt);
+    }
+}
