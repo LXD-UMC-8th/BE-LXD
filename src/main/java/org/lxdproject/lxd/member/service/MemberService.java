9cf57b3179558739341911a933f0c860f3ddc5f9
eclipse021 <yahang2008@naver.com>
2025-10-29 20:59:13 +0900

    Feat: MemberService에 List<SoftDeleteStrategy> 생성

--- src/main/java/org/lxdproject/lxd/member/service/MemberService.java
+++ src/main/java/org/lxdproject/lxd/member/service/MemberService.java
@@ -15,6 +15,7 @@ import org.lxdproject.lxd.member.converter.MemberConverter;
 import org.lxdproject.lxd.member.dto.*;
 import org.lxdproject.lxd.member.entity.Member;
 import org.lxdproject.lxd.member.repository.MemberRepository;
+import org.lxdproject.lxd.member.service.strategy.SoftDeleteStrategy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +38,8 @@ public class MemberService {
     private final DiaryCommentRepository diaryCommentRepository;
     private final DiaryLikeRepository diaryLikeRepository;
     private final DiaryCommentLikeRepository diaryCommentLikeRepository;
+
+    private final List<SoftDeleteStrategy> softDeleteStrategies;
 
     public Member join(MemberRequestDTO.JoinRequestDTO joinRequestDTO, MultipartFile profileImg) {
 
